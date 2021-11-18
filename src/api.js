e782796e8d597f2a0925b4b4d7e0fc73cddda416
Hamza <hamzatarar8@gmail.com>
2021-11-18 23:52:52 +0200

    Corrected linters

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export default class Api {
   static async moveToApi(player) {
-    const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Ml0wfxegyFf06umHiVlI/scores/', {
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Ml0wfxegyFf06umHiVlI/scores/', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
